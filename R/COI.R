ad3dece00a8414307a62ca511c64d351a8008e8b
Marc Choisy <mchoisy@oucru.org>
2024-08-21 21:03:56 +0700

    Fixing COI

--- R/COI.R
+++ R/COI.R
@@ -16,9 +16,15 @@ function(start = start, dt = dt, nc = nc, nr = nr, Period = Period, log2scale = 
   coi = fourier.factor * sqrt(2) * dt * c(1E-5, 1:((nc+1)/2-1), rev((1:(nc/2-1))), 1E-5)
   coi.x = c(axis.1[c(1, 1)] - dt*0.5, axis.1, axis.1[c(nc, nc)] + dt*0.5)
   logyint = axis.2[2] - axis.2[1]
-  yl = c(log2(Period[nr]) + 0.5*logyint, log2(Period[1]) - 0.5*logyint)
-  yr = rev(yl)
-  coi.y = c(yl, log2(coi), yr)
+  if (log2scale) {
+    yl = c(log2(Period[nr]) + 0.5*logyint, log2(Period[1]) - 0.5*logyint)
+    yr = rev(yl)
+    coi.y = c(yl, log2(coi), yr)
+  } else {
+    yl = c(Period[nr] + 0.5*logyint, Period[1] - 0.5*logyint)
+    yr = rev(yl)
+    coi.y = c(yl, coi, yr)
+  }
 
   # Output
   output <- list(x = coi.x, y = coi.y, axis.1 = axis.1, axis.2 = axis.2)
